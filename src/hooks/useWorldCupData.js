32249000f4bf88401948c0dc56b1b24ec375f81c
kessidiones <kessis01@gmail.com>
2026-06-17 19:15:56 -0300

    feat: add sync match

--- src/hooks/useWorldCupData.js
+++ src/hooks/useWorldCupData.js
@@ -55,9 +55,67 @@ export function useWorldCupData() {
           teamsMap[team.id] = team;
         });
 
+        // ----------------------------------------------------
+        // CALCULATE TABLE LOCALLY BASED ON COMPLETED GAMES
+        // To guarantee 100% accuracy even if API groups are lagging
+        // ----------------------------------------------------
+        const calculatedGroups = groupsData.groups.map(group => {
+          const teamStats = {};
+          group.teams.forEach(t => {
+            teamStats[t.team_id] = { ...t, mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: 0, pts: 0 };
+          });
+
+          gamesData.games.forEach(game => {
+             if (game.type !== 'group' || game.finished !== "TRUE") return;
+             
+             const homeId = game.home_team_id;
+             const awayId = game.away_team_id;
+             
+             // If both teams belong to this group
+             if (teamStats[homeId] && teamStats[awayId]) {
+               const homeScore = parseInt(game.home_score, 10);
+               const awayScore = parseInt(game.away_score, 10);
+
+               teamStats[homeId].mp += 1;
+               teamStats[homeId].gf += homeScore;
+               teamStats[homeId].ga += awayScore;
+               teamStats[homeId].gd = teamStats[homeId].gf - teamStats[homeId].ga;
+               
+               teamStats[awayId].mp += 1;
+               teamStats[awayId].gf += awayScore;
+               teamStats[awayId].ga += homeScore;
+               teamStats[awayId].gd = teamStats[awayId].gf - teamStats[awayId].ga;
+
+               if (homeScore > awayScore) {
+                 teamStats[homeId].w += 1;
+                 teamStats[homeId].pts += 3;
+                 teamStats[awayId].l += 1;
+               } else if (homeScore < awayScore) {
+                 teamStats[awayId].w += 1;
+                 teamStats[awayId].pts += 3;
+                 teamStats[homeId].l += 1;
+               } else {
+                 teamStats[homeId].d += 1;
+                 teamStats[homeId].pts += 1;
+                 teamStats[awayId].d += 1;
+                 teamStats[awayId].pts += 1;
+               }
+             }
+          });
+
+          // Sort teams
+          const sortedTeams = Object.values(teamStats).sort((a, b) => {
+            if (b.pts !== a.pts) return b.pts - a.pts;
+            if (b.gd !== a.gd) return b.gd - a.gd;
+            return b.gf - a.gf;
+          });
+
+          return { ...group, teams: sortedTeams };
+        });
+
         const newData = {
           teams: teamsMap,
-          groups: groupsData.groups,
+          groups: calculatedGroups,
           games: gamesData.games
         };
 
